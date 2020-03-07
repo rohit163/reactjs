67de91e61cb7f7ad4eb60aaa47836900ca941136
rohit163 <rohitphatarpekar@gmail.com>
2020-03-08 00:00:21 +0530

    somthing

---
+++ Desktop/react/react-complete-guide/src/Person/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import './Person.css';
+
+const person = ( props ) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+    return (
+        //<div className="Person" style={style}>
+        styled.div`  
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    )
+};
+
+export default person;
